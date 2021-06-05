76ae755c5c92c70033213ac7562ad2b212f7782e
Volodymyr <v.khalak96@gmail.com>
2021-06-05 22:57:40 +0300

    add My_WebApplication

---
+++ src/main/java/com/khalak/My_WebApplication.java
@@ -0,0 +1,13 @@
+package com.khalak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My_WebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(My_WebApplication.class, args);
+    }
+
+}
