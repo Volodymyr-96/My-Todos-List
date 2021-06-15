986e55be2437df9a1e260db8cbc2c848b5a46645
Volodymyr <v.khalak96@gmail.com>
2021-06-15 20:46:19 +0300

    update login page style and add logout to header

--- src/main/java/com/khalak/securityconfig/WebConfig.java
+++ src/main/java/com/khalak/securityconfig/WebConfig.java
@@ -11,7 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("helloPage");
         registry.addViewController("/login").setViewName("login");
     }
 
